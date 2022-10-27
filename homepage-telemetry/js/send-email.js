1e66a11ad73f1cc3ff4da892e12ff073bfbe1972
C349358 <frmartinez@santander.com.mx>
2022-10-26 21:13:43 -0500

    Se agrega boton para redireccionar al monitor

--- homepage-telemetry/js/send-email.js
+++ homepage-telemetry/js/send-email.js
@@ -1,45 +1,28 @@
 
 $(document).ready(function () {
 
+    $('#form_saved_data_contact').on('submit', function (event) {
+        if (!event.target.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
 
-    // $("#form_saved_data_contact").click(function () {
-    // 	var username = $("#username").val();
+        }
+        $(this).addClass('was-validated')
+    });
 
-    // 	$.ajax({
-    // 		url: "php/index.php/login",
-    // 		contentType: "application/x-www-form-urlencoded",
-    // 		type: "POST",
-    // 		dataType: 'json',
-    // 		data: { "username": username, "password": username },
+    // -----------------------------Guardar datos de contacto---------------------------
 
-    // 		success: function (responseText) {
-    // 			if (responseText.code != null) {
-    // 				window.location.href = "index.php";
-    // 				localStorage.setItem('username', JSON.stringify(responseText));
-    // 				document.cookie = "client= " + JSON.stringify(responseText);
+    $("#form_saved_data_contact").on('submit', function (event) {
 
-    // 			}
-    // 			if ( responseText.hasOwnProperty('status') ) {
-    // 				$("#error_message").text("Clave incorrecta");
-    // 			}
-
-    // 		},
-    // 		error: function (jqXHR, textStatus, errorThrown) {
-    // 			console.log(errorThrown + ': ' + textStatus);
-    // 			$("#username").val("");
-
-    // 		}
-    // 	});
-    // });
-
-
-
-    $("#form_saved_data_contact").submit(function () { // -----------------------------Guardar sucursal---------------------------
 
         var prospect_name = $("#prospect_name").val();
         var prospect_contact = $("#prospect_contact").val();
         var prospect_subject = $("#prospect_subject").val();
         var prospect_message = $("#prospect_message").val();
+
+        if (prospect_name == '' || prospect_contact == '' || prospect_subject == '' || prospect_message == '') {
+            return false;
+        }
 
         $.ajax({
             contentType: "application/json; charset=UTF-8",
@@ -53,38 +36,31 @@ $(document).ready(function () {
 
             success: function (response) {
                 console.log(response)
-                $('#form_saved_data_contact').each(function () {
-                    this.reset();
-                });
-                 swal("!    Muy bien ! ", "En breve nos pondremos en contacto con usted !", "success");
- 
-                if (responseText.hasOwnProperty('status')) {
-                    $("#error_message").text("Clave incorrecta");
+
+                if (response.hasOwnProperty('status')) {
+                    if (response.status == '200') {
+                        $('#form_saved_data_contact').each(function () {
+                            this.reset();
+                        });
+                        $('#form_saved_data_contact')
+                            .trigger("reset")
+                            .removeClass('was-validated')
+                        swal("!  Muy bien ! ", "En breve nos pondremos en contacto con usted !", "success");
+
+                    } else {
+                        swal("Intentelo más tarde!", "No se ha podido enviar el mensaje", "warning")
+
+                    }
                 }
 
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log(errorThrown + ': ' + textStatus);
-                swal("Intentelo más tarde!", "","warning")
+                swal("Intentelo más tarde!", "", "warning")
 
             }
         });
-        // $.ajax({
-        //     contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-        //     data: JSON.stringify({ "nombrePunto": "" + NombreSucursal + "", "ubicacion": "" + Ubicacion + "", "idFabrica": "" + idfab + "" }),
-        //     type: "POST",
-        //     dataType: 'json',
-        //     url: "" + ruta + "Create_Sucursal.php",
-        // })
-        //     .done(function (data) {
-        //         $("#nombreSuc").val("");
-        //         $("#ubicacionSuc").val("");
-        //         MostrarSucursales();
-        //         $.notify("Sucursal guardada", "success");
-        //     })
-        //     .fail(function (jqXHR, textStatus, errorThrown) {
-        //         console.log("jqxhr: " + jqXHR + ", Text Status: " + textStatus + ", errorThrown: " + errorThrown + "", "error");
-        //     });
-        return false;
-    }); //---------------------------------------fin guardar sucursal------------------------------------
+
+        return true;
+    }); //---------------------------------------Fin guardar datos contacto------------------------------------
 });
